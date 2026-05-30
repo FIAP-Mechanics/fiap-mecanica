a5e6a98d6263f5f4fe9c0968920046dd527f61f4
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2026-05-30 01:54:26 -0300

    feat: adiciona documentação basica swagger e remove cargo USER

--- src/main/java/com/fiap/mecanica/domain/Funcao.java
+++ src/main/java/com/fiap/mecanica/domain/Funcao.java
@@ -4,5 +4,4 @@ public enum Funcao {
     ADMIN,
     MECANICO,
     ATENDENTE,
-    USER
 }
--- src/main/java/com/fiap/mecanica/dto/FuncionarioDto.java
+++ src/main/java/com/fiap/mecanica/dto/FuncionarioDto.java
@@ -2,9 +2,16 @@ package com.fiap.mecanica.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fiap.mecanica.domain.Funcao;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-public record FuncionarioDto(Long id, String email, @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-String senha, String nome, Funcao funcao) {
+@Schema(description = "Dados do funcionário")
+public record FuncionarioDto(
+        @Schema(description = "Identificador único do funcionário", example = "1") Long id,
+        @Schema(description = "E-mail do funcionário", example = "joao@mecanica.com") String email,
+        @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        @Schema(description = "Senha do funcionário", accessMode = Schema.AccessMode.WRITE_ONLY) String senha,
+        @Schema(description = "Nome completo do funcionário", example = "João Silva") String nome,
+        @Schema(description = "Cargo do funcionário") Funcao funcao) {
 }
