88390fca890a08ad9bfbe34f7985974873dc7065
Pedro-Picorelli <86214970+Pedro-Picorelli@users.noreply.github.com>
2026-06-10 22:51:49 -0300

    fix: NOme para Nome

--- src/main/java/com/fiap/mecanica/dto/ClienteDto.java
+++ src/main/java/com/fiap/mecanica/dto/ClienteDto.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 @Builder
 @Schema(description = "Dados do cliente")
 public record ClienteDto(
-        @Schema(description = "NOme do cliente", example = "Jose da Silva") String nome,
+        @Schema(description = "Nome do cliente", example = "Jose da Silva") String nome,
         @Schema(description = "Identificador único do clinete", example = "1") Long id,
         @Schema(description = "CPF/CNPJ único do clinete", example = "123.456.789-99") String documento,
         @Schema(description = "E-mail único do clinete", example = "cliente@gmail.com") String email,
