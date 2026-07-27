3be2433f5c4aeaa52b60d5c20c9c518017928022
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2026-07-27 01:08:31 -0300

    feat: melhorias de clean code

--- src/main/java/com/fiap/mecanica/domain/Orcamento.java
+++ src/main/java/com/fiap/mecanica/domain/Orcamento.java
@@ -1,10 +1,7 @@
 package com.fiap.mecanica.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -28,14 +25,52 @@ public class Orcamento {
 
     @OneToMany(mappedBy = "orcamento", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
+    @ToString.Exclude
     private List<OrdemServicoServico> servicos = new ArrayList<>();
 
     @OneToMany(mappedBy = "orcamento", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
+    @ToString.Exclude
     private List<OrdemServicoInsumo> insumos = new ArrayList<>();
 
     @Column(name = "preco_total", precision = 10, scale = 2)
     private BigDecimal precoTotal;
+
+    public void adicionarServico(Servico servico, Integer quantidade) {
+        this.servicos.stream()
+                .filter(s -> s.getServico().getId().equals(servico.getId()))
+                .findFirst()
+                .ifPresentOrElse(
+                        itemExistente -> itemExistente.setQuantidade(itemExistente.getQuantidade() + quantidade),
+                        () -> {
+                            OrdemServicoServico novoItem = OrdemServicoServico.builder()
+                                    .orcamento(this)
+                                    .servico(servico)
+                                    .quantidade(quantidade)
+                                    .build();
+                            this.servicos.add(novoItem);
+                        }
+                );
+        recalcularPrecoTotal();
+    }
+
+    public void adicionarInsumo(Insumo insumo, Integer quantidade) {
+        this.insumos.stream()
+                .filter(i -> i.getInsumo().getId().equals(insumo.getId()))
+                .findFirst()
+                .ifPresentOrElse(
+                        itemExistente -> itemExistente.setQuantidade(itemExistente.getQuantidade() + quantidade),
+                        () -> {
+                            OrdemServicoInsumo novoItem = OrdemServicoInsumo.builder()
+                                    .orcamento(this)
+                                    .insumo(insumo)
+                                    .quantidade(quantidade)
+                                    .build();
+                            this.insumos.add(novoItem);
+                        }
+                );
+        recalcularPrecoTotal();
+    }
 
     public void recalcularPrecoTotal() {
         BigDecimal totalServicos = servicos.stream()
